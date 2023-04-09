5f55ad5865fa9ac2ebdb026502674eed16cdf3a1
Ondřej Nývlt <ondrej@nyv.lt>
2023-04-09 19:42:03 +0200

    Keep paragraph in context

--- pollen.rkt
+++ pollen.rkt
@@ -1,5 +1,5 @@
 #lang racket/base
-(require pollen/core pollen/decode pollen/misc/tutorial txexpr)
+(require pollen/core pollen/setup pollen/decode pollen/misc/tutorial txexpr)
 (provide (all-defined-out))
 
 ; Project metadata
@@ -13,6 +13,8 @@
 
 ; Transform output
 (define (root . elements)
-  (txexpr 'root empty (decode-elements elements
-    #:txexpr-elements-proc decode-paragraphs
-    #:string-proc (compose1 smart-quotes smart-dashes))))
+  (case (current-poly-target)
+    [(context) (txexpr 'root empty elements)]
+    [else (txexpr 'root empty (decode-elements elements
+      #:txexpr-elements-proc decode-paragraphs
+      #:string-proc (compose1 smart-quotes smart-dashes)))]))
